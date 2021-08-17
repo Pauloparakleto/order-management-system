2959e6d595b13b61bebaacb13e5250bc6fea9cce
Felipe <alparakleto@gmail.com>
2021-08-17 18:30:12 -0300

    refactor(order_spec.rb): add validates price and name

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
+def create_valid_order
+  Order.create(product_name: "PS3", price: 687.98)
+end
+
 RSpec.describe Order, type: :model do
 
   context "when increment" do
     it '1' do
-      order = Order.create
+      order = create_valid_order
       expect(order.order_number).to eq(1)
     end
 
     it '2' do
-      Order.create
-      order_second = Order.create
+      create_valid_order
+      order_second = create_valid_order
       expect(order_second.order_number).to eq(2)
     end
   end
 
   context "when create" do
     it 'valid' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       expect(order).to be_truthy
     end
 
@@ -34,52 +38,52 @@ RSpec.describe Order, type: :model do
     it 'has no price' do
       order = Order.create(product_name: "PS3")
       expect(order.errors.any?).to eq(true)
-      expect(order.errors[:base].pluck(:message)).to eq(["Forneça um preço"])
+      expect(order.errors.full_messages).to eq(["Price Forneça um preço"])
     end
 
     it 'has no product name' do
       order = Order.create(price: 9.99)
       expect(order.errors.any?).to eq(true)
-      expect(order.errors[:base].pluck(:message)).to eq(["Forneça um nome"])
+      expect(order.errors.full_messages).to eq(["Product name Forneça um nome"])
     end
 
     it 'status pending' do
-      order = Order.create(order_number: 1, status: :pending)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1, status: :pending)
       expect(order.status).to eq("pending")
     end
 
     it 'status pending as default' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       expect(order.status).to eq("pending")
     end
 
     it 'status completed' do
-      order = Order.create(order_number: 1, status: :completed)
+      order = Order.create(product_name: "PS3", price: 66.89, order_number: 1, status: :completed)
       expect(order.status).to eq("pending")
     end
 
     it 'status in progress' do
-      order = Order.create(order_number: 1, status: :in_progress)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1, status: :in_progress)
       expect(order.status).to eq("pending")
     end
   end
 
   context "when update" do
     it "pending to in_progress" do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.in_progress!
       expect(order.reload.status).to eq("in_progress")
     end
 
     it 'in progress to completed' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.in_progress!
       order.completed!
       expect(order.status).to eq("completed")
     end
 
     it 'in progress backward to pending' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.in_progress!
       order.update(status: :pending)
       expect(order.errors.any?).to eq(true)
@@ -87,7 +91,7 @@ RSpec.describe Order, type: :model do
     end
 
     it 'completed backward to pending' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.in_progress!
       order.completed!
       order.update(status: :pending)
@@ -96,7 +100,7 @@ RSpec.describe Order, type: :model do
     end
 
     it 'completed backward to in progress' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.in_progress!
       order.completed!
       order.update(status: :in_progress)
@@ -105,7 +109,7 @@ RSpec.describe Order, type: :model do
     end
 
     it 'pending to completed' do
-      order = Order.create(order_number: 1)
+      order = Order.create(product_name: "PS3", price: 234.56, order_number: 1)
       order.update(status: :completed)
       expect(order.errors.any?).to eq(true)
       expect(order.errors[:base].pluck(:message)).to eq(["Não é possível pular de pendente direto para completa!"])
