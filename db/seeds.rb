e253ca45dcea2ddac3a6a6d354c3ef8b781ccb45
Felipe <alparakleto@gmail.com>
2021-08-17 17:46:23 -0300

    chore(seeds.rb): add data population for orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+FactoryBot.create_list(:order, 20)
+
+index = Order.first.id.to_i
+
+10.times do
+  Order.find(index).in_progress!
+  index += 1
+end
+
+order_in_progress = Order.where(status: :in_progress)
+order_in_progress.first.completed!
+order_in_progress.second.completed!
+order_in_progress.last.completed!
+
