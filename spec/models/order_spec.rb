2334c29beab7ef955110540d5c9719c79c3870ba
Felipe <alparakleto@gmail.com>
2021-08-17 11:58:38 -0300

    test(models/order_spec.rb): add when update statuses

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe Order, type: :model do
       order = Order.create(order_number: 1, status: :completed)
       expect(order.status).to eq("pending")
     end
+
+    it 'status in progress' do
+      order = Order.create(order_number: 1, status: :in_progress)
+      expect(order.status).to eq("pending")
+    end
   end
 
   context "when update" do
@@ -32,6 +37,7 @@ RSpec.describe Order, type: :model do
 
     it 'in progress to completed' do
       order = Order.create(order_number: 1)
+      order.in_progress!
       order.completed!
       expect(order.status).to eq("completed")
     end
@@ -39,9 +45,34 @@ RSpec.describe Order, type: :model do
     it 'in progress backward to pending' do
       order = Order.create(order_number: 1)
       order.in_progress!
-      order.pending!
+      order.update(status: :pending)
       expect(order.errors.any?).to eq(true)
-      expect(order.errors[:base]).to eq(["Não é possível voltar order em progresso novamente para pendente!"])
+      expect(order.errors[:base].pluck(:message)).to eq(["Não é possível voltar order em progresso novamente para pendente!"])
+    end
+
+    it 'completed backward to pending' do
+      order = Order.create(order_number: 1)
+      order.in_progress!
+      order.completed!
+      order.update(status: :pending)
+      expect(order.errors.any?).to eq(true)
+      expect(order.errors[:base].pluck(:message)).to eq(["Não é possível alterar uma ordem já completa!"])
+    end
+
+    it 'completed backward to in progress' do
+      order = Order.create(order_number: 1)
+      order.in_progress!
+      order.completed!
+      order.update(status: :in_progress)
+      expect(order.errors.any?).to eq(true)
+      expect(order.errors[:base].pluck(:message)).to eq(["Não é possível alterar uma ordem já completa!"])
+    end
+
+    it 'pending to completed' do
+      order = Order.create(order_number: 1)
+      order.update(status: :completed)
+      expect(order.errors.any?).to eq(true)
+      expect(order.errors[:base].pluck(:message)).to eq(["Não é possível pular de pendente direto para completa!"])
     end
   end
 end
