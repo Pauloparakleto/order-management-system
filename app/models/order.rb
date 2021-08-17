659d5dd3fc4e3f26d23a24d15b04573fa2023bea
Felipe <alparakleto@gmail.com>
2021-08-17 16:13:36 -0300

    feat(models/order.rb): add set order number

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
+
   enum status: { pending: 0, in_progress: 1, completed: 2 }
 
   after_create :check_status
+
+  after_create :set_order_number
 
   validate :update_status, on: :update
 
@@ -11,17 +14,27 @@ class Order < ApplicationRecord
     self.pending! unless self.pending?
   end
 
+  def set_order_number
+    setting = if OrderSetting.first.nil?
+                OrderSetting.create(last_number: 0)
+              else
+                OrderSetting.first
+              end
+    self.order_number = OrderSetting.first.last_number + 1
+    setting.update!(last_number: self.order_number)
+  end
+
   def update_status
     if Order.find(self.id).in_progress? && self.pending?
-      errors.add(:base, message: "Não é possível voltar order em progresso novamente para pendente!")
+      errors.add(:base, message: 'Não é possível voltar order em progresso novamente para pendente!')
     end
 
     if Order.find(self.id).completed?
-      errors.add(:base, message: "Não é possível alterar uma ordem já completa!")
+      errors.add(:base, message: 'Não é possível alterar uma ordem já completa!')
     end
 
     if Order.find(self.id).pending? && self.completed?
-      errors.add(:base, message: "Não é possível pular de pendente direto para completa!")
+      errors.add(:base, message: 'Não é possível pular de pendente direto para completa!')
     end
   end
 end
