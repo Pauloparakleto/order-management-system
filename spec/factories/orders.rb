6613dc23064864e0088eabe2a2244cc7a1555c8d
Felipe <alparakleto@gmail.com>
2021-08-18 19:45:19 -0300

    chore(factories/orders.rb): add faker price and product names

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order do
-    order_number { nil }
-    product_name { "PS3" }
-    price { 334.98 }
-    status { nil }
+    order_number { 1 }
+    product_name { Faker::Commerce.product_name }
+    price { Faker::Commerce.price(range: 1..500.0) }
+    status { 0 }
   end
 end
